f70af6a06266afae3e9215852d23c0c912e33862
Greeny Don <greenydon198@gmail.com>
2023-05-13 16:55:48 +0530

    assign

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,8 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        cheese = 0;
+        top = 0;
         // your code goes here
     }
 }
